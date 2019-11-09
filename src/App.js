dac911d5c6ac0388d347d79d6c627978b0ece8b1
helmi <helmi@happy5.co>
2019-11-10 05:12:12 +0700

    draft layout login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './scss/_login.scss'
+import { Form, Button } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reloadaaaaa.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto mt-50" style={{ marginTop: '20vh' }}>
+          <div class="card card-signin my-5">
+            <div class="card-body">
+              <h5 class="card-title text-center">Promotion App Wika</h5>
+              <Form>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit" block>
+                  Submit
+              </Button>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
