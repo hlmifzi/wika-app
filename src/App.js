59ae0f348b8a32013366a9e2daaca41c0c896740
helmi <helmi@happy5.co>
2019-11-10 12:19:11 +0700

    login-layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import './scss/_login.scss'
 import { Form, Button } from 'react-bootstrap'
+import LoginLogo from './img/login-logo.jpeg'
 
 function App() {
   return (
     <div class="container">
       <div class="row">
-        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto mt-50" style={{ marginTop: '20vh' }}>
+        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto height-card" >
           <div class="card card-signin my-5">
             <div class="card-body">
-              <h5 class="card-title text-center">Promotion App Wika</h5>
+              <div class="card-title text-center">
+                <img className="login-wika" src={LoginLogo} alt="wika-logo" />
+              </div>
               <Form>
                 <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control type="email" placeholder="Username" />
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
@@ -26,6 +29,11 @@ function App() {
               </Form>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-9 col-md-7 col-lg-5 mx-auto text-center text-white">
+          Made with ♥ byThinkthin
         </div>
       </div>
     </div>
