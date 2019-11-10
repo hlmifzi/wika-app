fbc601e79622f39e5f91e0c8b91f16955b4a4e5a
helmi <helmi@happy5.co>
2019-11-10 13:03:27 +0700

    judul app

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './scss/_login.scss'
 import { Form, Button } from 'react-bootstrap'
 import LoginLogo from './img/login-logo.jpeg'
 
-function App() {
-  return (
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto height-card">
-          <div class="card card-signin my-5" style={{ borderRadius: '8px' }}>
-            <div class="card-body">
-              <div class="card-title text-center">
-                <img className="login-wika" src={LoginLogo} alt="wika-logo" />
-              </div>
-              <Form>
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Control type="email" placeholder="Username" />
-                </Form.Group>
+const App = () => {
+  const [Show, setShow] = useState(false)
 
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button variant="primary" type="submit" block>
-                  Submit
-              </Button>
-              </Form>
+  return (
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-9 col-md-7 col-lg-5 mx-auto height-card">
+            <div class="card card-signin my-5" style={{ borderRadius: '8px' }}>
+              <div class="card-body">
+                <div class="text-center">
+                  <img className="login-wika" src={LoginLogo} alt="wika-logo" />
+                </div>
+                <div class="card-title text-center">
+                  <b>HC DSU 1</b>
+                </div>
+                <Form>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Username" />
+                  </Form.Group>
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
+                  <Button variant="primary" type="submit" block>
+                    Submit
+                  </Button>
+                </Form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-9 col-md-7 col-lg-5 mx-auto text-center text-white">
-          Made with ♥ byThinkthin
+        <div className="row">
+          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto text-center text-white">
+            Made with ♥ byThinkthin
+        </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
