d259605110bdda33eac66083d46456e1ea6dfe5a
helmi <helmi@happy5.co>
2019-11-17 03:24:17 +0700

    spider chart

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,38 +1,52 @@
 import React, { Component } from 'react';
-import Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
+import ReactHighcharts from 'react-highcharts'
+import HighchartsMore from 'highcharts-more'
 
 import { Col, Row } from 'reactstrap';
 import Widget03 from '../Widgets/Widget03';
 import NavbarComponent from '../../MyComponent/Nav/NavbarComponent'
 import { CardWhiteComponent, CardDefaulSideProfiletComponent } from '../../MyComponent/CardCustom/CardComponent'
 
+HighchartsMore(ReactHighcharts.Highcharts)
+
+
 class Dashboard extends Component {
 
   render() {
-    const options = {
+    let config = {
 
       chart: {
         polar: true,
         type: 'line'
       },
 
-      accessibility: {
-        description: 'A spiderweb chart compares the allocated budget against actual spending within an organization. The spider chart has six spokes. Each spoke represents one of the 6 departments within the organization: sales, marketing, development, customer support, information technology and administration. The chart is interactive, and each data point is displayed upon hovering. The chart clearly shows that 4 of the 6 departments have overspent their budget with Marketing responsible for the greatest overspend of $20,000. The allocated budget and actual spending data points for each department are as follows: Sales. Budget equals $43,000; spending equals $50,000. Marketing. Budget equals $19,000; spending equals $39,000. Development. Budget equals $60,000; spending equals $42,000. Customer support. Budget equals $35,000; spending equals $31,000. Information technology. Budget equals $17,000; spending equals $26,000. Administration. Budget equals $10,000; spending equals $14,000.'
-      },
 
       title: {
-        text: 'Budget vs spending',
-        x: -80
+        text: 'Passing Grade vs Actual',
       },
 
       pane: {
-        size: '80%'
+        size: '100%'
       },
 
       xAxis: {
-        categories: ['Sales', 'Marketing', 'Development', 'Customer Support',
-          'Information Technology', 'Administration'],
+        categories: [
+          'IF',
+          'LA',
+          'IT',
+          'IN',
+          'CO',
+          'EM',
+          'CL',
+          'IP',
+          'PS',
+          'DE',
+          'DR',
+          'BA',
+          'SO',
+          'CF',
+          'BP',
+        ],
         tickmarkPlacement: 'on',
         lineWidth: 0
       },
@@ -45,7 +59,7 @@ class Dashboard extends Component {
 
       tooltip: {
         shared: true,
-        pointFormat: '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>'
+        pointFormat: '<span style="color:{series.color}">{series.name}: <b>{point.y:,.0f}</b><br/>'
       },
 
       legend: {
@@ -54,12 +68,12 @@ class Dashboard extends Component {
       },
 
       series: [{
-        name: 'Allocated Budget',
-        data: [43000, 19000, 60000, 35000, 17000, 10000],
+        name: 'Passing Grade',
+        data: [2, 2, 2, 3, 3, 3, 2, 2, 2, 2, 2, 3, 3, 3],
         pointPlacement: 'on'
       }, {
-        name: 'Actual Spending',
-        data: [50000, 39000, 42000, 31000, 26000, 14000],
+        name: 'Assessmen',
+        data: [3, 2, 1, 2, 4, 4, 2, 3, 4, 4, 3, 2, 4, 3],
         pointPlacement: 'on'
       }],
 
@@ -81,6 +95,7 @@ class Dashboard extends Component {
       }
 
     }
+
     return (
       <div className="animated fadeIn">
         <Row>
@@ -133,10 +148,13 @@ class Dashboard extends Component {
                 </Row>
               </Col>
             </CardWhiteComponent>
-
-            <div id="container-spider">
-              <HighchartsReact highcharts={Highcharts} options={options} />
-            </div>
+            <CardWhiteComponent text="Grafik Assessmen">
+              <Row>
+                <Col xs={12} sm={6} md={6}>
+                  <ReactHighcharts config={config} />
+                </Col>
+              </Row>
+            </CardWhiteComponent>
           </Col>
         </Row>
       </div >
