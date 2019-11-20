e61e2c162db1e7aef7d71c8d6bbbcf0f03d6af98
helmi <helmi@happy5.co>
2019-11-20 10:30:56 +0700

    revisi data

--- src/views/Dashboard/Assessmen.js
+++ src/views/Dashboard/Assessmen.js
@@ -1,7 +1,7 @@
 const usersData = [
-  { id: 1, name: 'Assessmen Job Fit', registered: '2018/01/01', role: 'ADW', status: 'Disarankan' },
-  { id: 1, name: 'Assessmen Job Target 1', registered: '2018/01/01', role: 'ADW', status: 'Disarankan' },
-  { id: 1, name: 'Assessmen Job Target 2', registered: '2018/01/01', role: 'ADW', status: 'Disarankan' },
+  { id: 1, name: 'Assessmen Job Fit', registered: '2018/01/01', role: 'LPTUI', status: 'Disarankan' },
+  { id: 1, name: 'Assessmen Job Target 1', registered: '2018/01/01', role: 'EXPERT', status: 'Disarankan Dengan Syarat' },
+  { id: 1, name: 'Assessmen Job Target 2', registered: '2018/01/01', role: 'LPTUI', status: 'Tidak Disarankan' },
 ]
 
 export default usersData
