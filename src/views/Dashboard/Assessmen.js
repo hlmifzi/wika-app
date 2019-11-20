9e3e1b27438d51cf3fc9fdd00a3841adc982ce4b
helmi <helmi@happy5.co>
2019-11-20 10:27:54 +0700

    push assessmen

--- src/views/Dashboard/Assessmen.js
+++ src/views/Dashboard/Assessmen.js
@@ -1,5 +1,7 @@
 const usersData = [
-  { id: 1, name: 'Assessmen Manager', registered: '2018/01/01', role: 'ADW', status: 'Disarankan' },
+  { id: 1, name: 'Assessmen Job Fit', registered: '2018/01/01', role: 'ADW', status: 'Disarankan' },
+  { id: 1, name: 'Assessmen Job Target 1', registered: '2018/01/01', role: 'ADW', status: 'Disarankan' },
+  { id: 1, name: 'Assessmen Job Target 2', registered: '2018/01/01', role: 'ADW', status: 'Disarankan' },
 ]
 
 export default usersData
