57e74ca7ad6573c7d262b37f9bbfda093addba67
helmi <helmi@happy5.co>
2020-01-04 00:15:17 +0700

    retry axios

--- src/MyServices/api/URLApi.js
+++ src/MyServices/api/URLApi.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
 import cookie from 'react-cookies'
 
 const urlBackend = 'http://www.mocky.io/'
@@ -13,28 +14,28 @@ const ROOT_API = axios.create({
 })
 
 export const client = async (endPoint, { method, params = "", body = "" }) => {
+   axiosRetry(ROOT_API, { retries: 3 });
+
    try {
       let getData
-      for (let i = 0; i < 3; i++) {
-         switch (method) {
-            case 'GET':
-               getData = await ROOT_API.get(endPoint, params)
-               break;
-            case 'POST':
-               getData = await ROOT_API.post(endPoint, body)
-               break;
-            case 'DELETE':
-               getData = await ROOT_API.delete(endPoint)
-               break;
-            case 'PATCH':
-               getData = await ROOT_API.patch(endPoint, body)
-               break;
-            default:
-               break;
-         }
-
-         if (getData.status === 200) return getData.data
+      switch (method) {
+         case 'GET':
+            getData = await ROOT_API.get(endPoint, params)
+            break;
+         case 'POST':
+            getData = await ROOT_API.post(endPoint, body)
+            break;
+         case 'DELETE':
+            getData = await ROOT_API.delete(endPoint)
+            break;
+         case 'PATCH':
+            getData = await ROOT_API.patch(endPoint, body)
+            break;
+         default:
+            break;
       }
+
+      if (getData.status === 200) return getData.data
    } catch (e) {
       alert(e.message)
    }
