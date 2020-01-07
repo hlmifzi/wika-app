b02f56d70139a91cb0419d80a14b5be5d890ba93
helmi <helmi@happy5.co>
2020-01-07 21:12:13 +0700

    rafcp

---
+++ src/views/DetailPegawai/DetailPegawai.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const DetailPegawai = props => {
+    return (
+        <div>
+            Detail Pegawai
+        </div>
+    )
+}
+
+DetailPegawai.propTypes = {
+
+}
+
+export default DetailPegawai
