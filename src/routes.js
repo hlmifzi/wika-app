2ea2339a56e2e207e1fc80015928992147106345
helmi <helmi@happy5.co>
2020-01-07 21:19:56 +0700

    tes

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-
 const Dashboard = React.lazy(() => import('./views/Dashboard/Dashboard'));
 const DataKaryawan = React.lazy(() => import('./views/Dashboard/DataKaryawan'));
+const DetailPegawai = React.lazy(() => import('./views/DetailPegawai/DetailPegawai'));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/filter-pegawai', exact: true, name: 'Filter Pegawai', component: DataKaryawan },
-  { path: '/karyawan/:id', exact: true, name: 'User Details', component: Dashboard },
+  { path: '/karyawan/:id', exact: true, name: 'User Details', component: DetailPegawai },
 ];
 
 export default routes;
