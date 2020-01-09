623cbcd76a123472b66cfb504bd1b0050cfa1539
arifinriski <arifintkj26@gmail.com>
2020-01-10 03:06:09 +0700

    try update layout

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Dashboard = React.lazy(() => import('./views/Dashboard/Dashboard'));
 const DataKaryawan = React.lazy(() => import('./views/Dashboard/DataKaryawan'));
 const DetailPegawai = React.lazy(() => import('./views/DetailPegawai/DetailPegawai'));
+// const DetailPegawai = React.lazy(() => import('./views/Widgets'));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
--- src/views/DetailPegawai/DetailPegawai.js
+++ src/views/DetailPegawai/DetailPegawai.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Col, Row, Progress, ListGroup, ListGroupItem, Badge, FormGroup, Label, Input } from 'reactstrap';
 import Widget03 from '../Widgets/Widget03';
-import NavbarComponent from '../../MyComponent/Nav/NavbarComponent'
-import PropTypes from 'prop-types'
-import { CardWhiteComponent, CardDefaulSideProfiletComponent } from '../../MyComponent/CardCustom/CardComponent'
+import Widget02 from '../Widgets/Widget02';
+// import NavbarComponent from '../../MyComponent/Nav/NavbarComponent'
+// import PropTypes from 'prop-types'
+import { CardWithCustom, CardWidget } from '../../MyComponent/CardCustom/CardComponent'
 
 const DetailPegawai = props => {
     return (
@@ -14,33 +15,132 @@ const DetailPegawai = props => {
                     </Widget03>
                 </Col>
                 <Col xs={12} sm={6} md={9}>
-                    <CardWhiteComponent text="Penilaian">
-                        <Col xs="12" md="12">
-                            <FormGroup row>
-                            <Col md="12">
-                                <Label htmlFor="selectSm">Nama</Label>
+                    <CardWithCustom classHeader={"bg-primary text-white"} text="Employement Status">
+                        <Row>
+                            <Col xs="12" md="6">
+                                <Col xs="12" md="12">
+                                    <FormGroup row>
+                                        <Col md="12" xs="12">
+                                            <Label htmlFor="selectSm">BOD Group</Label>
+                                        </Col>
+                                        <Col xs="12" md="12">
+                                            <Input type="text" id="bodGroup" placeholder="Group ***" required />
+                                        </Col>
+                                    </FormGroup>
+                                </Col>
+                                <Col xs="12" md="12">
+                                    <FormGroup row>
+                                        <Col md="12" xs="12">
+                                            <Label htmlFor="selectSm">Nama Jabatan</Label>
+                                        </Col>
+                                        <Col xs="12" md="12">
+                                            <Input type="text" id="nmJbtn" placeholder="Staff" required />
+                                        </Col>
+                                    </FormGroup>
+                                </Col>
+                                <Col xs="12" md="12">
+                                    <FormGroup row>
+                                        <Col md="12" xs="12">
+                                            <Label htmlFor="selectSm">Status Jabatan</Label>
+                                        </Col>
+                                        <Col xs="12" md="12">
+                                            <Input type="text" id="stsJbtn" placeholder="Berjalan" required />
+                                        </Col>
+                                    </FormGroup>
+                                </Col>
+                                <Col xs="12" md="12">
+                                    <FormGroup row>
+                                        <Col md="12" xs="12">
+                                            <Label htmlFor="selectSm">Tgl Menjabat Ditetapkan</Label>
+                                        </Col>
+                                        <Col xs="12" md="12">
+                                            <Input type="date" id="tglMnjbtTtpkn" required />
+                                        </Col>
+                                    </FormGroup>
+                                </Col>
+                                <Col xs="12" md="12">
+                                    <FormGroup row>
+                                        <Col md="12" xs="12">
+                                            <Label htmlFor="selectSm">Tgl Menjabat Berlaku</Label>
+                                        </Col>
+                                        <Col xs="12" md="12">
+                                            <Input type="date" id="tglMnjbtBrlku" required />
+                                        </Col>
+                                    </FormGroup>
+                                </Col>
                             </Col>
-                            <Col xs="12" md="12">
-                                <Input type="number" id="name" placeholder="Nama Karyawan" onChange={(e) => this.setState({ nama: e.target.value })} required />
+                            <Col xs="12" md="6">
+                                <Col xs="12" md="12">
+                                    <FormGroup row>
+                                        <Col md="12" xs="12">
+                                            <Label htmlFor="selectSm">Lama Menjabat</Label>
+                                        </Col>
+                                        <Col xs="12" md="12">
+                                            <Input type="text" id="lmMnjbt" required />
+                                        </Col>
+                                    </FormGroup>
+                                </Col>
+                                <Col xs="12" md="12">
+                                    <FormGroup row>
+                                        <Col md="12" xs="12">
+                                            <Label htmlFor="selectSm">Masa Kerja</Label>
+                                        </Col>
+                                        <Col xs="12" md="12">
+                                            <Input type="text" id="MsKrj" required />
+                                        </Col>
+                                    </FormGroup>
+                                </Col>
+                                <Col xs="12" md="12">
+                                    <FormGroup row>
+                                        <Col md="12" xs="12">
+                                            <Label htmlFor="selectSm">Projek JO/NON JO</Label>
+                                        </Col>
+                                        <Col xs="12" md="12">
+                                            <Input type="text" id="prjkType" required />
+                                        </Col>
+                                    </FormGroup>
+                                </Col>
+                                <Col xs="12" md="12">
+                                    <FormGroup row>
+                                        <Col md="12" xs="12">
+                                            <Label htmlFor="selectSm">Besaran Proyek</Label>
+                                        </Col>
+                                        <Col xs="12" md="12">
+                                            <Input type="text" id="bsrnPrjk" required />
+                                        </Col>
+                                    </FormGroup>
+                                </Col>
+                                <Col xs="12" md="12">
+                                    <FormGroup row>
+                                        <Col md="12" xs="12">
+                                            <Label htmlFor="selectSm">Jenis Projek Pernah Ditangani</Label>
+                                        </Col>
+                                        <Col xs="12" md="12">
+                                            <Input type="select" name="jnsPrjkPrnhDtgni" id="jnsPrjkPrnhDtgni" multiple>
+                                                <option value="1">Railway</option>
+                                                <option value="2">Tol</option>
+                                                <option value="3">Industrial Plant</option>
+                                            </Input>
+                                        </Col>
+                                    </FormGroup>
+                                </Col>
                             </Col>
-                            </FormGroup>
+                        </Row>
+                    </CardWithCustom>
+                    <Row>
+                        <Col xs="12" sm="6" lg="3">
+                            <Widget02 header="$1.999,50" mainText="Income" icon="fa fa-cogs" color="primary" variant="1" />
                         </Col>
-                        <Col xs="12" md="3">
-                            <FormGroup row>
-                            <Col md="12">
-                                <Label htmlFor="selectSm">Masa jabatan</Label>
-                            </Col>
-                            <Col xs="12" md="12">
-                                <Input type="select" name="select" id="select">
-                                <option value="0">Pilih</option>
-                                <option value="1">Lebih Dari</option>
-                                <option value="2">Kurang Dari</option>
-                                <option value="3">Sama Dengan</option>
-                                </Input>
-                            </Col>
-                            </FormGroup>
+                        <Col xs="12" sm="6" lg="3">
+                            <Widget02 header="$1.999,50" mainText="Income" icon="fa fa-laptop" color="info" variant="1" />
                         </Col>
-                    </CardWhiteComponent>
+                        <Col xs="12" sm="6" lg="3">
+                            <Widget02 header="$1.999,50" mainText="Income" icon="fa fa-moon-o" color="warning" variant="1" />
+                        </Col>
+                        <Col xs="12" sm="6" lg="3">
+                            <Widget02 header="$1.999,50" mainText="Income" icon="fa fa-cogs" color="primary" variant="1" />
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </div>
