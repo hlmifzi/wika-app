61cacb14e7c9c4de60eceded6e28123a61eb7633
helmi <helmi@happy5.co>
2020-01-14 20:15:17 +0700

    fix

--- src/views/DetailPegawai/DetailPegawai.js
+++ src/views/DetailPegawai/DetailPegawai.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {useState} from 'reinspect'
+import React, { useEffect } from 'react'
+import { useState } from 'reinspect'
 import { Col, Row, Badge, FormGroup, Label, Input, Button, TabContent, TabPane } from 'reactstrap';
 import Widget02 from '../Widgets/Widget02';
 import WidgetCustom from '../Widgets/WidgetCustom';
@@ -13,38 +13,69 @@ import { DetailInformation } from './DetailInformation.js';
 import { SideProfile } from './SideProfile.js';
 
 const DetailPegawai = props => {
-    const [navbar1, setNavbar1] = useState(false, "nav1");
+    const [navbar1, setNavbar1] = useState(true, "nav1");
     const [navbar2, setNavbar2] = useState(false, "nav2");
     const [navbar3, setNavbar3] = useState(false, "nav3");
     const [navbar4, setNavbar4] = useState(false, "nav4");
+
+    const _personal = () => {
+        setNavbar1(true)
+        setNavbar2(false)
+        setNavbar3(false)
+        setNavbar4(false)
+    }
+
+    const _job = () => {
+        setNavbar1(false)
+        setNavbar2(true)
+        setNavbar3(false)
+        setNavbar4(false)
+    }
+
+    const _training = () => {
+        setNavbar1(false)
+        setNavbar2(false)
+        setNavbar3(true)
+        setNavbar4(false)
+    }
+    const _performance = () => {
+        setNavbar1(false)
+        setNavbar2(false)
+        setNavbar3(false)
+        setNavbar4(true)
+    }
+
+    useEffect(() => {
+
+    }, [navbar1, navbar2, navbar3, navbar4])
     return (
         <div>
             <Row>
                 <Col xs={12} sm={6} md={3}>
                     <WidgetCustom dataBox={() => ({ variant: 'twitter' })} >
                     </WidgetCustom>
-                    <SideProfile></SideProfile>
+                    <SideProfile />
                 </Col>
                 <Col xs={12} sm={6} md={9}>
-                        <Row className="mb-2">
-                            <Col xs={12} sm={12} md={12} className="text-center">
-                                <Button onClick={()=> setNavbar1(true)} size="md" className="btn-facebook btn-brand text mr-2"><span>Personal</span></Button>
-                                <Button onClick={()=> setNavbar2(true)} size="md" className="btn-facebook btn-brand text mr-2"><span>Job</span></Button>
-                                <Button onClick={()=> setNavbar3(true)} size="md" className="btn-facebook btn-brand text mr-2"><span>Training</span></Button>
-                                <Button onClick={()=> setNavbar4(true)} size="md" className="btn-facebook btn-brand text"><span>Performance</span></Button>
-                            </Col>
-                        </Row>
+                    <Row className="mb-2">
+                        <Col xs={12} sm={12} md={12} className="text-center">
+                            <Button onClick={_personal} size="md" className="btn-facebook btn-brand text mr-2"><span>Personal</span></Button>
+                            <Button onClick={_job} size="md" className="btn-facebook btn-brand text mr-2"><span>Job</span></Button>
+                            <Button onClick={_training} size="md" className="btn-facebook btn-brand text mr-2"><span>Training</span></Button>
+                            <Button onClick={_performance} size="md" className="btn-facebook btn-brand text"><span>Performance</span></Button>
+                        </Col>
+                    </Row>
                     {
-                       navbar1 &&  
+                        navbar1 &&
                         <BasicInformation></BasicInformation>
-                    } 
+                    }
                     {
                         navbar2 &&
 
                         <DetailInformation></DetailInformation>
                     }
                     {
-                        
+
                     }
                 </Col>
             </Row>
