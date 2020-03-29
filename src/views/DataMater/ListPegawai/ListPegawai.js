4585ee0def66d5b631be7f5a603df4f57d55540a
helmi <helmi@happy5.co>
2020-03-29 20:11:16 +0700

    kebalik

--- src/views/DataMater/ListPegawai/ListPegawai.js
+++ src/views/DataMater/ListPegawai/ListPegawai.js
@@ -142,10 +142,10 @@ const ListPegawai = ({ match }) => {
             <CardHeader>
               <i className="fa fa-users"></i> Daftar Seluruh Karyawan {`${filter.type} ${filter.field}`}
               <button style={btnUploadFile} onClick={() => setShowModal(true)}>Import User</button>
-              <a href="http://api.dedekrnwan.site/user/template/excel">
+              <a href="http://api.dedekrnwan.site/user/report/profile">
                 <button style={btnDownloadFile} ><i class="fa fa-file"></i>&nbsp;Download Profile Pegawai</button>
               </a>
-              <a href="http://api.dedekrnwan.site/user/report/profile">
+              <a href="http://api.dedekrnwan.site/user/template/excel">
                 <button style={btnDownloadFile}><i class="fa fa-file"></i>&nbsp; Download Posisi Pegawai</button><br />
               </a>
             </CardHeader>
