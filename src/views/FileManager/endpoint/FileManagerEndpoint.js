6202b8ba390dc8eb15f7c495c4159f9c720ccd14
khainan <muhammadrachim.k@gmail.com>
2020-04-12 13:50:35 +0700

    add edit function on file management

--- src/views/FileManager/endpoint/FileManagerEndpoint.js
+++ src/views/FileManager/endpoint/FileManagerEndpoint.js
@@ -4,4 +4,5 @@ const endPointFileManager = 'fileManagement'
 const endPointUpload = 'fileManagement?parentId=1'
 
 export const getFileManager = () => clientGet(endPointFileManager, {})
+export const getFileManagementById = (id) => clientGet(`${endPointFileManager}/${id}`, {})
 export const uploadFile = (body) => clientPost(endPointFileManager, body)
