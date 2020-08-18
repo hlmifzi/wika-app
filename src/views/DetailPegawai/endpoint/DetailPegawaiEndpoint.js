41a4c842f500934a0870e561d972b04296fb0f1b
helmi <helmi@happy5.co>
2020-08-19 01:11:25 +0700

    query status active

--- src/views/DetailPegawai/endpoint/DetailPegawaiEndpoint.js
+++ src/views/DetailPegawai/endpoint/DetailPegawaiEndpoint.js
@@ -2,6 +2,6 @@ import { clientGet, clientPatch } from '../../../MyServices/api/URLApi'
 
 const endPointDataPegawai = 'user/'
 
-export const getDataPegawai = (id) => clientGet(`${endPointDataPegawai}${id}`, {})
+export const getDataPegawai = id => clientGet(`${endPointDataPegawai}${id}`, {})
 export const updateDataPegawai = (id, body) => clientPatch(`${endPointDataPegawai}${id}`, body)
-export const getRangkapan = (id) => clientGet(`user-position/getByUserId/${id}`, {})
+export const getRangkapan = id => clientGet(`user-position/getByUserId/${id}`, { params: { status:'active' } })
